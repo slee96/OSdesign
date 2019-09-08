156a856fd50ee29463e8987dc5a4ea68c9a1902f
SpencerLee11 <spencerealee@gmail.com>
2019-09-08 00:41:44 -0400

    In the if loop, set counter equal to the index of character to be removed. Created a while loop to terminate one character before the end of the string is reached. Shift the string to the left starting at the index of character to be removed. After the while loop exits, I decrement the string variable and set the last character in the string to null

--- assignment_1/1.3.c
+++ assignment_1/1.3.c
@@ -10,8 +10,16 @@ char *rmchr(char *string, char *character){
   for (int i = 0; i < length; i++){
     // If statement to remoed character from the string at index[i] if its the same as the character to search for
     if (character[0] == string[i]){
-      // Code to remove caracter to come
-      
+      // set counter equal to the index of found character
+      int counter = i;
+      length --;
+      while(string[counter + 1] != '\0'){
+        // shift the string starting at counter
+        string[counter] = string[counter +1];
+        counter++;
+      }
+      // writes null character to the end of the string
+      string[length -1] = '\0';
     }
   }
   return string;
