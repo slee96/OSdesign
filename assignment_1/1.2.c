80ddfc165796db7c49923b1ac0559e4f52a7bbf6
SpencerLee11 <spencerealee@gmail.com>
2019-09-08 00:53:00 -0400

    Removed my code from merge 1.2.c

--- assignment_1/1.2.c
+++ assignment_1/1.2.c
@@ -1,38 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 // GCD-Function
 int gcd(int x, int y){
-	
+
 	if (x == 0 ){
-		return y;	
+		return y;
 	}
 	return gcd(y % x, x);
 }
-
-<<<<<<< HEAD
-	//for loop to itterate through command line arguments
-	for (int i = 1; i < argc; i++ ){
-		
-		//printf("%d\n", atoi(argv[i])); -- testing
-=======
 // PHI-Function
 int phi(int a){
 
-	int output = 1;	     
-	for ( int i = 2; i < a; i++){	
+	int output = 1;
+	for ( int i = 2; i < a; i++){
 		if(gcd(i,a) == 1 ){
 				output++;
-			} } 
+			} }
 	return output;
 }
 
 int main(int argc, char *argv[]){
 	//itterate through commandline arguments
 	for (int i = 1; i < argc; i++ ){
-	// print output of phi 
+	// print output of phi
 	printf("%s%d%s%d\n", "phi(" ,atoi(argv[i]),"):",phi(atoi(argv[i])));
->>>>>>> refs/remotes/origin/master
 	}
 }
