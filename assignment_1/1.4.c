5bf3f1f67dca26c62c0062b4a4cf606523b1b6cd
SpencerLee11 <spencerealee@gmail.com>
2019-09-08 01:39:30 -0400

    -- 1.3.c -- Fixed incrementation problem with for loop, it wasnt catching recurring charaters (e.g. $ ./1.3.c.out food o 											$ fod ) -- 1.4.c -- Created a for loop to iterate through the characters to remove in the string. This for loop is place within the for loop iterating throught the string. Each charater in the string, will be compared with each character to remove

--- assignment_1/1.4.c
+++ assignment_1/1.4.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 #include <string.h>
 
-char *rmchr(char *string, char *character){
-  // create int to store the value of the string length
-  int length = strlen(string);
-  // for loop to itterate throught the string
-  for (int i = 0; i < length; i++){
-    // If statement to remoed character from the string at index[i] if its the same as the character to search for
-    if (character[0] == string[i]){
-      // set counter equal to the index of found character
-      int counter = i;
-      while(string[counter + 1] != '\0'){
-        // shift the string starting at counter
-        string[counter] = string[counter +1];
-        counter++;
+char *rmchr(char *string, char *characters){
+  // for loop to itterate throught the string's
+  for (int i = 0; i < strlen(string); i++){
+    for (int j = 0; j < strlen(characters); j++){
+      // If statement to remoed character from the string at index[i] if its the same as the character to search for
+      if (characters[j] == string[i]){
+        // set counter equal to the index of found character
+        int counter = i;
+        j = 0;
+        i--;
+        while(string[counter] != '\0'){
+          // shift the string starting at counter
+          string[counter] = string[counter + 1];
+          counter++;
+        }
       }
-      // writes null character to the end of the string
-      length --;
-      string[length -1] = '\0';
     }
   }
   return string;
