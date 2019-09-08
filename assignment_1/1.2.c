a81c9c7a66f90a3d9a157309027249d7f6e38496
SpencerLee11 <spencerealee@gmail.com>
2019-09-08 00:27:30 -0400

    Created int to store the value of the string length For loop to itterate throught the string If statement to remoed character from the string at index[i] if its the same as the character to search for

--- assignment_1/1.2.c
+++ assignment_1/1.2.c
@@ -4,9 +4,9 @@
 
 int main(int argc, char *argv[]){
 
-	//itterate through commandline arguments
+	//for loop to itterate through command line arguments
 	for (int i = 1; i < argc; i++ ){
-
+		
 		//printf("%d\n", atoi(argv[i])); -- testing
 	}
 	return 0;
--- assignment_1/1.3.c
+++ assignment_1/1.3.c
@@ -3,13 +3,20 @@
 #include <math.h>
 #include <string.h>
 
-char *rmchar(char *string, char *character){
-  printf("%c\n", string[0]);
-  printf("%s\n", character);
-
-  return "result";
+char *rmchr(char *string, char *character){
+  // create int to store the value of the string length
+  int length = strlen(string);
+  // for loop to itterate throught the string
+  for (int i = 0; i < length; i++){
+    // If the character to remove is equal character at index[i] remove the character
+    if (character[0] == string[i]){
+      // Code to remove caracter to come
+      
+    }
+  }
+  return string;
 }
 
 int main(int argc, char *argv[]){
-  printf("%s\n", rmchar(argv[1], argv[2]));
+  printf("%s\n", rmchr(argv[1], argv[2]));
 }
