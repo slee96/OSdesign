41e34cdec3512504c8233c362122de7dea59c408
Ken <42424491+hadoukenan@users.noreply.github.com>
2019-09-07 23:41:42 -0400

    added recursive function to calculate the gcd of two integers used in checking for relative prime numbers
    added phi function to calculate the number of integers not exceeding n
    that are relativey prime to n

--- assignment_1/1.2.c
+++ assignment_1/1.2.c
@@ -7,7 +7,47 @@ int main(int argc, char *argv[]){
 	//itterate through commandline arguments
 	for (int i = 1; i < argc; i++ ){
 
-		//printf("%d\n", atoi(argv[i])); -- testing
-	}
-	return 0;
+	//	printf("%d\n","", atoi(argv[i])); -- testing
+
+	}	
+	return 0
+
+
+
 }
+
+
+// Recursive GCD-Function 
+int gcd(int x, int y){
+	
+	if (x == 0 ){
+		return y;
+	}
+	return gcd(y % x, x);
+}
+
+// Phi-Function
+  int phi(int a){
+
+         int output = 1;
+         for ( int i = 2; i < a; i++){			
+             
+		 if(gcd(i,a) == 1){                            
+			 outcome++;
+                         
+		 }}
+       	 return outcome;      
+  }
+
+
+
+
+
+
+
+
+
+
+		
+} 
+
