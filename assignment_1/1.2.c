1a800d90eaf511de8ac3e4dce69f0b64441a6f2b
Ken <42424491+hadoukenan@users.noreply.github.com>
2019-09-08 00:11:20 -0400

    added print statement to be used per argument passed

--- assignment_1/1.2.c
+++ assignment_1/1.2.c
@@ -2,52 +2,30 @@
 #include <stdlib.h>
 #include <math.h>
 
-int main(int argc, char *argv[]){
-
-	//itterate through commandline arguments
-	for (int i = 1; i < argc; i++ ){
-
-	//	printf("%d\n","", atoi(argv[i])); -- testing
-
-	}	
-	return 0
-
-
-
-}
-
-
-// Recursive GCD-Function 
+// GCD-Function
 int gcd(int x, int y){
 	
 	if (x == 0 ){
-		return y;
+		return y;	
 	}
 	return gcd(y % x, x);
 }
 
-// Phi-Function
-  int phi(int a){
+// PHI-Function
+int phi(int a){
 
-         int output = 1;
-         for ( int i = 2; i < a; i++){			
-             
-		 if(gcd(i,a) == 1){                            
-			 outcome++;
-                         
-		 }}
-       	 return outcome;      
-  }
+	int output = 1;	     
+	for ( int i = 2; i < a; i++){	
+		if(gcd(i,a) == 1 ){
+				output++;
+			} } 
+	return output;
+}
 
-
-
-
-
-
-
-
-
-
-		
-} 
-
+int main(int argc, char *argv[]){
+	//itterate through commandline arguments
+	for (int i = 1; i < argc; i++ ){
+	// print output of phi 
+	printf("%s%d%s%d\n", "phi(" ,atoi(argv[i]),"):",phi(atoi(argv[i])));
+	}
+}
