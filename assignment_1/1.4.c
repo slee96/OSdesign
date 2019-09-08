fd0a6925849f2441fd9664fe06c8cf457c2fad1e
SpencerLee11 <spencerealee@gmail.com>
2019-09-08 00:54:52 -0400

    copy 1.3.c to 1.4.c as a baseline

---
+++ assignment_1/1.4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+char *rmchr(char *string, char *character){
+  // create int to store the value of the string length
+  int length = strlen(string);
+  // for loop to itterate throught the string
+  for (int i = 0; i < length; i++){
+    // If statement to remoed character from the string at index[i] if its the same as the character to search for
+    if (character[0] == string[i]){
+      // set counter equal to the index of found character
+      int counter = i;
+      while(string[counter + 1] != '\0'){
+        // shift the string starting at counter
+        string[counter] = string[counter +1];
+        counter++;
+      }
+      // writes null character to the end of the string
+      length --;
+      string[length -1] = '\0';
+    }
+  }
+  return string;
+}
+
+int main(int argc, char *argv[]){
+  printf("%s\n", rmchr(argv[1], argv[2]));
+}
