293e6e6f1bee146a82e5d3b62b59eabdc64a231f
Ken <42424491+hadoukenan@users.noreply.github.com>
2019-09-07 22:42:29 -0400

    Create 1.2.c

---
+++ 1.2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, char *argv[]){
+	
+ int k = atoi(argv[1]), p = atoi(argv[2]), q = atoi(argv[3]);
+
+ int array[];
+ 
+ 
+ 
+ 
+ 
+}
