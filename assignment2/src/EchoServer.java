41ae26757b3973e7c18dfb1864f2072b501af745
SpencerLee <spacecar@localhost.localdomain>
2019-09-16 11:16:44 -0400

    Created Thread for server socket port

--- assignment2/src/EchoServer.java
+++ assignment2/src/EchoServer.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+// File Name GreetingServer.java
+import java.net.*;
+import java.io.*;
 
-public class EchoServer {
+public class EchoServer extends Thread {
+    private ServerSocket serverSocket;
+
+    public EchoServer(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+        serverSocket.setSoTimeout(20000);
+    }
+
+    public void run() {
+        while(true) {
+            
+        }
+    }
+
+    public static void main(String [] args) {
+        try {
+            Thread t = new EchoServer(8080);
+            t.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
