ecc04b330aef71bc798e22136744b119578ccb6a
ken <6b656e@localhost.localdomain>
2019-10-02 01:23:32 -0400

    Added pipe functionality between the parent and child process. Will be used in order to pass the buffer containing the file.

--- assignment2/filecopy.c
+++ assignment2/filecopy.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h> 
 #include <sys/types.h>
 #include <sys/stat.h>
 
 int main(int argc, char * argv[]){
-
+    // pd = Pipe Direction
+    int pd[2];
     //Child PID
     pid_t childpid;
+    //buffer for file
+    char buffer[filesize]
+
+    //Create Pipe
+    pipe(pd);
 
     //Fork & Error Handling
     if((childpid = fork()) == -1){
@@ -15,16 +22,21 @@ int main(int argc, char * argv[]){
     
     }//Child Start
     if(childpid == 0){
-   	  
-	    // test
-	    //printf("im son");
+	    
+	    //Closes Output (only input needed) 
+	    close(pd[1]);
+	    //Read buffer from input 
+	    read(pd[0],buffer,sizeof(buffer)); 
 
     }//Child End
     else
     {//Parent Start
-            //test
-   	    // printf("im dad");
-    
+	    
+	    //Closes Input (only output needed)
+      	    close(pd[0]);
+	    //Writes String to Pipe
+            write(pd[1],buffer,sizeof(buffer)); 	    
+
     }//Parent End
 
     return 0;
