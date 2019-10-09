79da0c7abe976b5fafcf6bc9f6f02f1880b4d9a4
root <root@kali.pi.hole>
2019-10-09 16:35:20 -0400

    Removed Single thread declarition from main method Moved infinate while loop from run() to main() To attempt to create a new thread for each connection

--- assignment_3/EchoServer.java
+++ assignment_3/EchoServer.java
@@ -2,36 +2,38 @@ import java.net.*;
 import java.io.*;
 
 public class EchoServer extends Thread {
-    private ServerSocket serverSocket;
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(8080);
 
-    public static void main(String[] args) {
-        try {
-            Thread t = new EchoServer(8080);
-            t.start();
-        } catch (IOException e) {
-            e.printStackTrace();
+        while (true){
+            Socket socket = null;
+            try{
+                socket = serverSocket.accept();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
         }
-    }
 
-    public EchoServer(int port) throws IOException {
-        serverSocket = new ServerSocket(port);
     }
 
     public void run() {
-        BufferedReader reader;
-        PrintWriter output;
+        //BufferedReader reader;
+        //PrintWriter output;
         try {
             // server infinite loop
-            while (true) {
-                Socket socket = serverSocket.accept();
-                reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                output = new PrintWriter(socket.getOutputStream(), true);
+            //while (true) {
+                //Socket socket = serverSocket.accept();
+                //reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                //output = new PrintWriter(socket.getOutputStream(), true);
 
                 // Response/Echo infinite loop
                 while (true) {
                     output.println("Server: " + reader.readLine());
                 }
-            }
+            //}
         } catch (IOException e) {
             e.printStackTrace();
         }
