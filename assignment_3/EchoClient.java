40bc1d09efc4d35b3ef017c6cb3c2c91a27d0a98
root <root@kali.pi.hole>
2019-10-09 17:03:56 -0400

    Added error handling for EchoClient Error messages are thrown when: 	No hostname is given 	Invalid hostname

--- assignment_3/EchoClient.java
+++ assignment_3/EchoClient.java
@@ -4,14 +4,23 @@ import java.io.*;
 public class EchoClient {
 
     public static void main(String [] args) {
-        String serverName = args[0];
-        Socket socket = null;
+        String serverName = null;
+        try {
+            serverName = args[0];
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Error: No hostname specified in the first argument");
+            System.out.println("usage: EchoClient arg1");
+            return;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
+        Socket socket = null;
         BufferedReader serverReader = null;
         BufferedReader stdinReader = null;
         PrintWriter output = null;
 
-        try{
+        try {
             socket = new Socket(serverName, 8080);
             serverReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             stdinReader = new BufferedReader(new InputStreamReader(System.in));
@@ -20,16 +29,23 @@ public class EchoClient {
 
             while (true) {
                 input = stdinReader.readLine();
-                if(input.equals(".")) break;
+                if (input.equals(".")) break;
                 output.println(input);
                 System.out.print(serverReader.readLine() + "\n");
             }
+        }catch (UnknownHostException e) {
+            System.out.println("Invalid Host, please specify a valid hostname");
+            return;
         } catch (IOException e) {
             e.printStackTrace();
         }finally {
             try {
                 socket.close();
-            } catch (IOException e) {e.printStackTrace();}
+            }catch (NullPointerException e){
+                System.out.println("Cannot close connection to 'NULL' host");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
 
     }
