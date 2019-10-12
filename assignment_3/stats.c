ba2de3cf52472a33d4ec37b549ae941f0b5d9966
Ken <42424491+hadoukenan@users.noreply.github.com>
2019-10-12 14:33:39 -0400

    Added overall program flow in TO-DO style comments

---
+++ assignment_3/stats.c
@@ -0,0 +1,67 @@
+//INCLUDE NECESSARY LIBS 
+
+
+
+//Create Global Vars for Stats ( max,min,avg )
+
+//Create Function Prototypes for calculations
+
+//Get Average takes cl args
+//Get Minimum takes cl args
+//Get Maximum takes cl args
+//Create Error Check Function to Verify CL Args are Correct
+
+//Need to create a struct to pass CL args due to
+//being limited in the # of params available for a thread function
+
+
+
+
+
+int main (int argc, char *argv[]){
+
+//First loop through the CL Arguments and verify they are numbers
+
+
+
+
+//Create 3 Seperate Threads
+
+
+//Run Each Thread 
+
+
+//Print Results 
+
+
+
+//Exit Sucessfully 
+
+
+}
+
+
+//Get Average
+
+	//loop through nums and sum
+	//divide by # of nums
+	//set global var avg
+	
+//Get Minimum 
+	//loop through nums 
+	//compare and keep smallest num in a var
+	//set global var min as var
+	
+//Get Maximum
+
+	//loop through nums
+	//compare and keep largest num in a var
+	//set global var max as var
+
+	
+//Verify CL	Args are Numbers 
+
+
+	//loop through args
+	//check if the args are all numbers
+    //return 1 if correct 0 if not correct 
